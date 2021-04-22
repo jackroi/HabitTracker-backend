952eb83c83efaa02f68d4cadff59f1bf4512881f
Giacomo Rosin <giacomorosin@gmail.com>
2021-04-23 01:05:21 +0200

    Start implementing server.ts

---
+++ server.ts
@@ -0,0 +1,83 @@
+/**
+ *
+ *
+ */
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors = require('cors');
+
+// add colorization to logs
+import 'console-info';
+import 'console-warn';
+import 'console-error';
+
+
+// ResponseBody types
+interface ResponseBody {
+  statusCode: number;
+}
+
+interface ErrorResponseBody extends ResponseBody {
+  error: boolean;
+  errorMessage: string;
+}
+
+
+const app = express();
+
+// CORS
+app.use(cors());
+
+// automatically parse json body
+app.use(express.json());
+
+// logging middleware
+app.use((req, _, next) => {
+  console.info('------------------------------------------------');
+  console.info(`New request for: ${req.url}` );
+  console.info(`Method: ${req.method}`);
+  next();
+});
+
+// API routes
+
+app.get('/', (_, res) => {
+  const jsonResponse = {
+    apiVersion: '0.0.1',    // TODO store version somewhere else
+    endpoints: [
+      '/',
+    ],
+  };
+  res.status(200).json(jsonResponse);
+});
+
+
+// error handling middleware
+app.use((err: ErrorResponseBody, _: Request, res: Response, __: NextFunction) => {
+  console.error('Request error:');
+  console.error(JSON.stringify(err, null, 2));
+
+  res.status(err.statusCode).json(err);
+});
+
+
+// last middleware (request with invalid endpoint)
+app.use((_, res) => {
+  console.warn('Invalid endpoint');
+  const errorResponse: ErrorResponseBody = {
+    statusCode: 404,
+    error: true,
+    errorMessage: 'Invalid endpoint',
+  };
+  res.status(404).json(errorResponse);
+});
+
+
+
+
+
+import http = require('http');                  // HTTP module
+
+let server = http.createServer(app);
+
+server.listen(8080, () => console.info("HTTP Server started on port 8080"));
