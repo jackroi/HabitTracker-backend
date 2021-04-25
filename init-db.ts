f5610e18e1785d7cc51bd206002007f1cfa14392
Giacomo Rosin <giacomorosin@gmail.com>
2021-04-25 18:49:11 +0200

    Add init-db.ts

---
+++ init-db.ts
@@ -0,0 +1,102 @@
+import mongoose from 'mongoose';
+// add colorization to logs
+import 'console-info';
+import 'console-warn';
+import 'console-error';
+
+import * as user from './User';
+// import { } from './User';
+import * as habit from './Habit';
+import { Habit } from './Habit';
+import * as historyEntry from './HistoryEntry';
+import { HistoryEntry, HistoryEntryType } from './HistoryEntry';
+
+
+// TODO maybe put db name and other inside .env
+const DB_URL = 'mongodb://habit-tracker-db:27017/habittracker';
+
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+.then(() => {
+  console.info('Connected to MongoDB');
+})
+.then(() => {
+  console.info('Dropping database');
+
+  return mongoose.connection.db.dropDatabase();
+})
+.then(() => {
+  console.info('Creating test user');
+
+  const testUser = {
+    name: 'test',
+    email: 'test@gmail.com',
+    password: 'test',
+    registrationDate: new Date(2021, 0, 10),
+  }
+  const testUserDocument = user.newUser(testUser);
+  return testUserDocument.save();
+})
+.then((testUserDocument) => {
+  console.info('Creating habits for test user');
+
+  const testUserHabits: Habit[] = [
+    {
+      name: 'Study',
+      creationDate: new Date(2021, 1, 2),
+      category: 'Learning',
+      archived: false,
+      userId: testUserDocument._id,
+    },
+    {
+      name: 'Run',
+      creationDate: new Date(2021, 2, 5),
+      category: 'Sport',
+      archived: false,
+      userId: testUserDocument._id,
+    },
+    {
+      name: 'Read',
+      creationDate: new Date(2021, 2, 7),
+      category: 'Learning',
+      archived: false,
+      userId: testUserDocument._id,
+    },
+  ];
+  return habit.getModel().insertMany(testUserHabits);
+}).then((habitDocuments) => {
+  console.info('Creating history entries for test user habits');
+
+  const historyEntries: HistoryEntry[] = [];
+  for (let habitDocument of habitDocuments) {
+    historyEntries.push({
+      habitId: habitDocument._id,
+      date: new Date(2021, 3, 1),
+      type: HistoryEntryType.COMPLETED,
+    });
+    historyEntries.push({
+      habitId: habitDocument._id,
+      date: new Date(2021, 3, 2),
+      type: HistoryEntryType.SKIPPED,
+    });
+    historyEntries.push({
+      habitId: habitDocument._id,
+      date: new Date(2021, 3, 3),
+      type: HistoryEntryType.COMPLETED,
+    });
+  }
+  return historyEntry.getModel().insertMany(historyEntries);
+}).then(() => {
+  return mongoose.connection.close();
+})
+.then(() => {
+  console.info('Database correctly initialized');
+})
+.catch((err) => {
+  console.error('Error occurred during initialization');
+  console.error(err);
+});
