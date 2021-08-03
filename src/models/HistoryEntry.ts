147906af865a5689734ca099dad4286706b013cc
Giacomo Rosin <giacomorosin@gmail.com>
2021-08-03 23:18:34 +0200

    HistoryEntry now is coded to be embedded

--- src/models/HistoryEntry.ts
+++ src/models/HistoryEntry.ts
@@ -8,7 +8,6 @@ export enum HistoryEntryType {
 }
 
 export interface HistoryEntry {
-  habitId: mongoose.Schema.Types.ObjectId;  // TODO valutare se rinominarlo in 'habit'
   date: Date;
   type: HistoryEntryType;
 }
@@ -16,27 +15,8 @@ export interface HistoryEntry {
 export interface HistoryEntryDocument extends HistoryEntry, mongoose.Document {
 }
 
-export interface HistoryEntryModel extends mongoose.Model<HistoryEntryDocument> {
-}
 
-// type guard
-// TODO capire se serve
-export const isHistoryEntry = (arg: any): arg is HistoryEntry => {
-  return arg
-    && arg.habitId
-    && arg.habitId instanceof mongoose.Schema.Types.ObjectId
-    && arg.date
-    && arg.date instanceof Date
-    && arg.type
-    && (arg.type === HistoryEntryType.COMPLETED || arg.type === HistoryEntryType.SKIPPED);
-};
-
-
-const HistoryEntrySchema = new mongoose.Schema<HistoryEntryDocument, HistoryEntryModel>({
-  habitId: {
-    type: mongoose.SchemaTypes.ObjectId,
-    required: true,
-  },
+export const HistoryEntrySchema = new mongoose.Schema({
   date: {
     type: mongoose.SchemaTypes.Date,
     required: true,
@@ -52,11 +32,3 @@ const HistoryEntrySchema = new mongoose.Schema<HistoryEntryDocument, HistoryEntr
 export const getSchema = () => {
   return HistoryEntrySchema;
 };
-
-let historyEntryModel: HistoryEntryModel;
-export const getModel = (): HistoryEntryModel => {
-  if (!historyEntryModel) {
-    historyEntryModel = mongoose.model<HistoryEntryDocument, HistoryEntryModel>('HistoryEntry', getSchema());
-  }
-  return historyEntryModel;
-}
