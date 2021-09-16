ea3201ad70d9a5f58f235f8e2527beea8b9b5952
Giacomo Rosin <giacomorosin@gmail.com>
2021-09-16 11:35:32 +0200

    Add utils.ts with validateEmail (regex)

---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * @param email
+ * @returns
+ */
+export function validateEmail(email: string): boolean {
+  const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  return regex.test(email);
+}
